f7009091182f2d28e37b628f30467f5c16ea20b8
Anant Shukla <anantshukla404@gmail.com>
2024-07-14 11:08:33 +0530

    database configuration logic

--- config/db.js
+++ config/db.js
@@ -1,19 +1,31 @@
 const mongoose = require('mongoose');
+const {Sequelize} = require('sequelize');
+
+// using sequelize as the DB
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: './db.sqlite3'
+})
+
+sequelize.sync();
 
 const connectDB = async () => {
 
     try {
         await mongoose.connect('mongodb+srv://devandy44:MJLKiDDciBgmmuEA@cluster04.sval3ig.mongodb.net/?retryWrites=true&w=majority&appName=Cluster04', {
-
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            tlsInsecure: true 
         });
 
         console.log('MongoDB connected');
     }
     catch(err){
-
         console.error(err.message);
         process.exit(1);
     }
 }
 
-module.exports = connectDB;
+
+
+module.exports = sequelize;
