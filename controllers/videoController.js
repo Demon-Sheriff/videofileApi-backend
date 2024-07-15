0056faa65826a378e6779ecb547f531e354a88b1
Anant Shukla <anantshukla404@gmail.com>
2024-07-15 13:48:17 +0530

    installing dependency fluent-ffmpeg

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -99,4 +99,27 @@ exports.generateLink = async (req, res) =>{
     }
 };
 
+// get all videos
+exports.getAllVideos = async (req, res) => {
+    try {
+        const videos = await Video.find();
+        res.json(videos);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+// get a single video by id
+exports.getVideoById = async (req, res) => {
+    try {
+        const video = await Video.findById(req.params.id);
+        if (!video) {
+            return res.status(404).json({ error: 'Video not found' });
+        }
+        res.json(video);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
 
